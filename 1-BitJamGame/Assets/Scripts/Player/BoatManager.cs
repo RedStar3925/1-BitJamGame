e4ca6de2d668eb4025e509a940a4d713cc86cc15
pampppp <mousspamp1@outlook.be>
2024-10-01 20:35:31 +0200

    some update
    add boatmanagerscrypt
    add textmeshpro
    update test scenne
    add temp boat sprite1 & 2
    add temp button sprite
    remove old scrypt i made we dont need

---
+++ 1-BitJamGame/Assets/Scripts/Player/BoatManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatManager : MonoBehaviour
+{
+    public GameObject curentsentry; //just for testing script
+
+    private Transform _currentSqrSelected;
+
+    public void SetCurrentSqrSelected(Transform trm) => _currentSqrSelected = trm;
+    public Transform GetCurrentSqrSelected() => _currentSqrSelected;
+    
+    
+
+
+    public void RepairSentry()
+    {
+        //if money && hp < maxhp {tower hp = tower hp max; playermoney -= repairprice} (repair price will be set by the amount of hp restored)
+        //play sound repairturet
+    }
+    public void RemoveSentry()
+    {
+        //playermoney += turetcost/2
+        Destroy(_currentSqrSelected.GetComponentInChildren<GameObject>());
+        //play sound refound turret
+    }
+    public void TestBuySentry()
+    {
+        
+        GameObject t = Instantiate(curentsentry,_currentSqrSelected.position,Quaternion.identity,_currentSqrSelected);
+    }
+    public void BuySentry(GameObject sentry)
+    {
+        //if playermoney > cost
+        //playermoney -= cost
+        GameObject t = Instantiate(sentry,_currentSqrSelected.position,Quaternion.identity,_currentSqrSelected);
+        //playsound buyturet
+    }
+    public void UpgradeSentry()
+    {
+        //if playermoney > upgradecost
+        //sqr.GetComponentInChildren<Sentry>()).UpdateSentry();
+        //playsound upgrad
+    }
+}
