c214ce62ce9c8f3661d66ae3021b8d44e295330a
chatnoir95 <ferrari.a95@hotmail.fr>
2024-10-02 04:28:56 +0200

    new spawn enemy
    Enemies will spawn at the top and bottom of the screen, and the spawn location will be accurate regardless of the camera length.
    
    new off screen clean enemy

--- 1-BitJamGame/Assets/Scripts/Enemies/BasicEnemieScript.cs
+++ 1-BitJamGame/Assets/Scripts/Enemies/BasicEnemieScript.cs
@@ -22,6 +22,7 @@ public class BasicEnemieScript : MonoBehaviour
     {
         MoveOnPlayer();
         transform.Translate(scrollingDirection.normalized * Time.deltaTime * scrollingSpeed, Space.World);
+        CleanOffScreen();
     }
 
     public void Init()
@@ -44,4 +45,33 @@ public class BasicEnemieScript : MonoBehaviour
             transform.Translate(dirToPlayer.normalized * Time.deltaTime * enemyType.speed, Space.World);
         }
     }
+
+    private void CleanOffScreen() // destroy enemy when they go to far away of the camera
+    {
+        Camera camera = Camera.main;
+        Vector2 topLeft = camera.ViewportToWorldPoint(new Vector2(0, 1));
+        if (transform.position.x <= (topLeft.x *1.2f))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    //public void EnemyTakeDamage(int dmg = 10)
+    //{
+    //    life -= dmg;
+    //    if (life <= 0)
+    //    {
+    //        // add a coin 
+    //        Destroy(this.gameObject);
+    //    }
+    //}
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (collision.tag == "Bullet")
+    //    {
+    //        EnemyTakeDamage();
+    //        Destroy(collision.gameObject);
+    //    }
+    //}
 }
--- 1-BitJamGame/Assets/Scripts/Enemies/spawnEnemies.cs
+++ 1-BitJamGame/Assets/Scripts/Enemies/spawnEnemies.cs
@@ -6,7 +6,7 @@ public class spawnEnemies : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] GameObject enemyBase;
-    [SerializeField] Transform[] transformsSpawn; 
+    [SerializeField] Transform[] transSpawnStatic, transSpawnGround, transSpawnWater; 
 
     public float cdSpawnEnemy;
     [SerializeField] List<EnemyScriptableObject> allTypeOfEnemies;
@@ -23,30 +23,46 @@ public class spawnEnemies : MonoBehaviour
 
     public void SpawnEnemy(EnemyScriptableObject enemyScripObj) // call this will spawn a ennemy then it will launch a timer before recall this methode 
     {
-        GameObject enemy = Instantiate(enemyBase, transformsSpawn[0]);
+        GameObject enemy = Instantiate(enemyBase);
         BasicEnemieScript basicEnemieScript = enemy.GetComponent<BasicEnemieScript>();
         basicEnemieScript.enemyType = enemyScripObj;
-        
+
+        System.Random random = new System.Random();
         switch (enemyScripObj.enemyBehavior)
         {
+
+
             case EnemyBehavior.stayOnLand:
 
-                System.Random random = new System.Random();
-                int a = random.Next(0, transformsSpawn.Length); // chose where the enemy will spawn
-                enemy.transform.SetParent(transformsSpawn[a]);
+                int a = random.Next(0, transSpawnStatic.Length); // chose where the enemy will spawn
+                enemy.transform.SetParent(transSpawnStatic[a]);
 
                 
                 // add a random range for the enemy spawn 
                 float transY = Random.Range(-0.5f, 0.5f);
+
+                enemy.transform.position = transSpawnStatic[a].position;
                // Debug.Log(transY);
-                Vector2 startingPosition = enemy.GetComponent<Transform>().position;
-                startingPosition.y = transformsSpawn[a].position.y + transY;
+                Vector2 startingPosition = enemy.transform.position;
+                startingPosition.y = transSpawnStatic[a].position.y + transY;
 
                 enemy.GetComponent<Transform>().position = startingPosition;
                 break;
 
             case EnemyBehavior.moveOnLand:
+                a = random.Next(0, transSpawnGround.Length); // chose where the enemy will spawn
+                enemy.transform.SetParent(transSpawnGround[a]);
+                //Debug.Log(transSpawnGround[a]);
 
+                // add a random range for the enemy spawn 
+                float transX = Random.Range(-5f, 5f);
+
+                enemy.transform.position = transSpawnGround[a].position;
+                // Debug.Log(transY);
+                Vector2 startingPositionGround = enemy.transform.position;
+                startingPositionGround.x = transSpawnGround[a].position.x + transX;
+
+                enemy.GetComponent<Transform>().position = startingPositionGround;
                 break;
 
             case EnemyBehavior.moveOnWater:
@@ -57,6 +73,7 @@ public class spawnEnemies : MonoBehaviour
         basicEnemieScript.Init();
         StartCoroutine(StartCooldownSpawn());
     }
+
 
 
     IEnumerator StartCooldownSpawn() // spawn timer 
@@ -81,9 +98,26 @@ public class spawnEnemies : MonoBehaviour
 
     }
 
+    private void PlaceAllSpawn()
+    {
+        Camera camera = Camera.main;
+        Vector2 midleTop = camera.ViewportToWorldPoint(new Vector2(0.5f, 1.2f));
+        transSpawnGround[0].position = midleTop;
+
+        Vector2 midleBot = camera.ViewportToWorldPoint(new Vector2(0.5f, -0.2f));
+        transSpawnGround[1].position = midleBot;
+
+        Vector2 RightTop = camera.ViewportToWorldPoint(new Vector2(1.2f, 0.9f));
+        transSpawnStatic[0].position = RightTop;
+
+        Vector2 RightBot = camera.ViewportToWorldPoint(new Vector2(1.2f, 0.1f));
+        transSpawnStatic[1].position = RightBot;
+    }
+
     void Start()
     {
         SpawnEnemy(allTypeOfEnemies[0]);
+        PlaceAllSpawn();
     }
 
 }
